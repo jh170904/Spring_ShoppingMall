ee53f57b0226ab4e4e87835e168fcb823841bfd8
jeonghee <heui6166@gmail.com>
2019-05-13 12:28:23 +0900

    테스트용으로 반영한 게시판 삭제 ProductDAO 임포트 내용 삭제

--- SpringShoppingMall/src/main/java/com/codi/dao/ProductDAO.java
+++ SpringShoppingMall/src/main/java/com/codi/dao/ProductDAO.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.codi.dto.ProductDTO;
-import com.codi.dto.TestDTO;
 
 @Component("productDAO")
 public class ProductDAO {
